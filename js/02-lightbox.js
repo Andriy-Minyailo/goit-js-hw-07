9b344f49398407e2e3b2e00023d97defe88abba8
Andrii Minyailo <andrii.minyailo@gmail.com>
2023-03-06 23:41:44 +0200

    add task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
 
-console.log(galleryItems);
+galleryContainer.innerHTML = galleryItems.map(({preview, original, description}) => 
+        `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+</a></li>`).join("");
+
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
+
+
+
+
+
